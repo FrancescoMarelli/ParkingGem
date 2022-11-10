1b2bde9630af1fe88ff339f66f17112ae747348b
FrancescoMarelli <alu0101161730@ull.edu.es>
2022-11-10 18:54:32 +0000

    Controlando errores función status

--- spec/Parking_spec.rb
+++ spec/Parking_spec.rb
@@ -1,4 +1,7 @@
 RSpec.describe Parking do
+  before :all do
+
+  end
 
      it "Tiene un número de version: #{Parking::VERSION}" do
        expect(Parking::VERSION).not_to be nil
@@ -14,10 +17,12 @@ RSpec.describe Parking do
         
         it "Se cuenta con una funcion para mostrar el estado de un aparcamiento (completo, plazas libres)" do
         expect(Parking::status(10)).to eq(Parking::IS_FREE)
+        expect { Parking::status("10")}.to raise_error("Argument is incorrect") #strings
+        expect { Parking::status(-10) }.to raise_error("Argument is incorrect")  #negativos
         end
       end
 
-  context "Parking::Data" do
+  context "Tiene una clase para almacenar los datos del aparcamiento" do
      it "does something useful" do
        expect(true).to eq(true)
      end

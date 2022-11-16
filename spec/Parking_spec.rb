369dbee63a0496a5aaef83494029b6e0d4b7e457
FrancescoMarelli <alu0101161730@ull.edu.es>
2022-11-16 20:03:20 +0000

    Expectativas Parking sea objeto de Module #Funciona

--- spec/Parking_spec.rb
+++ spec/Parking_spec.rb
@@ -7,6 +7,9 @@ RSpec.describe Parking do
     @parking_spot = Parking::ParkingSpot.new(1.0,2.8,1.9)
     @p = Parking::Data.new(001,"Suarez", "Cubierto", "motos".to_s, 50, 50, 5, 6, @parking_spot)
     @q = Parking::Data.new(002,"Cristo Diaz","Aire libre", "coches", 50, 10, 3, 10, @parking_spot)
+    @r = Parking::Data.new(003,"Cristo ","Mixto", "autobuses", 50, 0, 3, 10, @parking_spot)
+    @status = Parking::Functions.new(@p)
+    @statusp2 = Parking::Functions.new(@q)
 
     #Objetos p8
     # Creando dos vehiculos, uno de ellos es un motor
@@ -18,7 +21,6 @@ RSpec.describe Parking do
     @m2 = Parking::Motor.new(4, 5, 250, 130)
     @m3 = Parking::Motor.new(4, 5, 500, 160)
 
-
   end
 
 #
@@ -26,6 +28,9 @@ RSpec.describe Parking do
 #  EXPECTATIVAS DE LA CLASE VEHICULO
 #
 #
+    it "Tiene un número de version: #{Parking::VERSION}" do
+      expect(Parking::VERSION).not_to be nil
+    end
 
   context "Representación de un Vehiculo - Parking::Vehiculo" do
 
@@ -325,98 +330,78 @@ RSpec.describe Parking do
 #
 #
 
-     it "Tiene un número de version: #{Parking::VERSION}" do
-       expect(Parking::VERSION).not_to be nil
-      end
+      context "Representación de los Datos de un Aparcamiento - Parking::Data" do
+         context "Atributos de la clase Parking::Data" do
+          context "Tiene una clase para almacenar los datos del aparcamiento" do
+            it "Costructor" do
+              expect(@p).not_to eq(nil)
+              expect(@p).not_to eq(@q)
+              expect(@p).to eq(@p)
+            end
 
-      context "Funcionalidades" do
-
-        it "Se cuenta con una constante para representar si el aparcamiento está completo" do
-          expect(Parking::IS_COMPLETE).to eq("Parking is already full")
-        end
-
-        it "Se cuenta con una constante para representar si el aparcamiento tiene plazas libres" do
-          expect(Parking::IS_FREE).to eq("Parking still has empty spots")
-        end
-        
-        it "Se cuenta con una funcion para mostrar el estado de un aparcamiento (completo, plazas libres)" do
-        expect(Parking::status(10)).to eq(Parking::IS_FREE)
-        expect { Parking::status("10")}.to raise_error("Argument is incorrect") #strings
-        expect { Parking::status(-10) }.to raise_error("Argument is incorrect")  #negativos
-        end
-      end
-
-      context "Parking::Data" do
-        context "Tiene una clase para almacenar los datos del aparcamiento" do
-          it "Costructor" do
-            expect(@p).not_to eq(nil)
-            expect(@p).not_to eq(@q)
-            expect(@p).to eq(@p)
-          end
-
-           it "Todo aparcamiento tiene el atributo de accesibilidad (1..5)" do
-            expect(@p.accessibility).to eq(5)
-            expect(@p.accessibility).not_to eq(3)
-            expect { @p.accessibility(-1) }.to raise_error(ArgumentError) #negativos
-            expect { @p.accessibility(6) }.to raise_error(ArgumentError) #fuera de rango
-            expect { @p.accessibility("diez") }.to raise_error(ArgumentError) #String
-            expect { @p.accessibility(1.0) }.to raise_error(ArgumentError) #Float    
-           end
+            it "Todo aparcamiento tiene el atributo de accesibilidad (1..5)" do
+              expect(@p.accessibility).to eq(5)
+              expect(@p.accessibility).not_to eq(3)
+              expect { @p.accessibility(-1) }.to raise_error(ArgumentError) #negativos
+              expect { @p.accessibility(6) }.to raise_error(ArgumentError) #fuera de rango
+              expect { @p.accessibility("diez") }.to raise_error(ArgumentError) #String
+              expect { @p.accessibility(1.0) }.to raise_error(ArgumentError) #Float    
+            end
            
 
-           it "Todo aparcamiento tiene el atributo seguridad(1..10)" do
-            expect(@p.security).to eq(6)
-            expect(@p.security).not_to eq(3)
-            expect { @p.security(-1) }.to raise_error(ArgumentError)
-            expect { @p.security(11) }.to raise_error(ArgumentError)
-            expect { @p.security("diez") }.to raise_error(ArgumentError)
-            expect { @p.security(1.0) }.to raise_error(ArgumentError)   
-           end
+            it "Todo aparcamiento tiene el atributo seguridad(1..10)" do
+              expect(@p.security).to eq(6)
+              expect(@p.security).not_to eq(3)
+              expect { @p.security(-1) }.to raise_error(ArgumentError)
+              expect { @p.security(11) }.to raise_error(ArgumentError)
+              expect { @p.security("diez") }.to raise_error(ArgumentError)
+              expect { @p.security(1.0) }.to raise_error(ArgumentError)   
+            end
 
-           it "Un aparcamiento tiene un atributo para su identificación" do
-           expect(@p.id).to eq(001)
-           expect(@p.id).not_to eq(000)
-           expect { @p.id(-1) }.to raise_error(ArgumentError)
-           expect { @p.id("diez") }.to raise_error(ArgumentError)
-           expect { @p.id(1.0) }.to raise_error(ArgumentError)  
-          end
+            it "Un aparcamiento tiene un atributo para su identificación" do
+              expect(@p.id).to eq(001)
+              expect(@p.id).not_to eq(000)
+              expect { @p.id(-1) }.to raise_error(ArgumentError)
+              expect { @p.id("diez") }.to raise_error(ArgumentError)
+              expect { @p.id(1.0) }.to raise_error(ArgumentError)  
+            end
 
-          it "Un aparcamiento tiene un atributo para su nombre comercial" do
-          expect(@p.name).to eq("Suarez")
-          expect(@p.name).not_to eq("Houston")
-          expect { @p.name(-1) }.to raise_error(ArgumentError)
-          expect { @p.name(1.0) }.to raise_error(ArgumentError)
-          end
+            it "Un aparcamiento tiene un atributo para su nombre comercial" do
+              expect(@p.name).to eq("Suarez")
+              expect(@p.name).not_to eq("Houston")
+              expect { @p.name(-1) }.to raise_error(ArgumentError)
+              expect { @p.name(1.0) }.to raise_error(ArgumentError)
+            end
 
-          it "Un aparcamiento tiene un atributo para su descripción (Cubierto - Aire libre - Mixto)" do
-             expect((@p.desc).is_a? Numeric).to eq(false)
-             expect((@p.desc).is_a? String).to eq(true)
-             expect((@p.desc).is_a? Hash).to eq(false)
-             expect((@p.desc).is_a? Array).to eq(false)
-           end
+            it "Un aparcamiento tiene un atributo para su descripción (Cubierto - Aire libre - Mixto)" do 
+              expect((@p.desc).is_a? Numeric).to eq(false)
+              expect((@p.desc).is_a? String).to eq(true)
+              expect((@p.desc).is_a? Hash).to eq(false)
+              expect((@p.desc).is_a? Array).to eq(false)
+            end
 
-           it "Tiene un atributo para el tipo de aparcamiento (autobuses, bicicletas, coches, motos)" do
-             expect((@p.type).is_a? Numeric).to eq(false)
-             expect((@q.type).is_a? String) #porque es nil? y no string
-             expect((@p.type).is_a? Hash).to eq(false)
-             expect((@p.type).is_a? Array).to eq(false)
-           end
+            it "Tiene un atributo para el tipo de aparcamiento (autobuses, bicicletas, coches, motos)" do
+              expect((@p.type).is_a? Numeric).to eq(false)
+              expect((@q.type).is_a? String) #porque es nil? y no string
+              expect((@p.type).is_a? Hash).to eq(false)
+              expect((@p.type).is_a? Array).to eq(false)
+            end
 
-           it "Tiene un atributo para representar el conjunto de plazas del aparcamiento (altura, longitud, anchura)" do 
-           expect((@parking_spot.width).is_a? Numeric).to eq(true)
-           expect((@parking_spot.length).is_a? Numeric).to eq(true)
-           expect((@parking_spot.height).is_a? Numeric).to eq(true)
+            it "Tiene un atributo para representar el conjunto de plazas del aparcamiento (altura, longitud, anchura)" do 
+              expect((@parking_spot.width).is_a? Numeric).to eq(true)
+              expect((@parking_spot.length).is_a? Numeric).to eq(true)
+              expect((@parking_spot.height).is_a? Numeric).to eq(true)
 
-           expect((@parking_spot.width).is_a? String).to eq(false)
-           expect((@parking_spot.length).is_a? String).to eq(false)
-           expect((@parking_spot.height).is_a? String).to eq(false)
-           end
+              expect((@parking_spot.width).is_a? String).to eq(false)
+              expect((@parking_spot.length).is_a? String).to eq(false)
+              expect((@parking_spot.height).is_a? String).to eq(false)
+            end
 
-           it "Tiene un atributo para representar el conjunto de plazas libres y ocupadas" do 
-             expect(@p.n_freespots).to eq(50)
-             expect((@p.n_freespots).is_a? Integer).to eq(true)
-             expect(@p.oc_spots).to eq(0)
-             expect(@q.oc_spots).to eq(40)
+            it "Tiene un atributo para representar el conjunto de plazas libres y ocupadas" do 
+              expect(@p.n_freespots).to eq(50)
+              expect((@p.n_freespots).is_a? Integer).to eq(true)
+              expect(@p.oc_spots).to eq(0)
+              expect(@q.oc_spots).to eq(40)
             end
 
             it "Tiene un metodo para devolver el numero de plazas del aparcamiento" do
@@ -432,4 +417,49 @@ RSpec.describe Parking do
           end
         end
       end
+
+      context "Interfaz de las funcionalidades - Parking::Functions" do
+
+        it "Se cuenta con una constante para representar si el aparcamiento está completo" do
+          expect(Parking::IS_COMPLETE).to eq("Parking is already full")
+        end
+
+        it "Se cuenta con una constante para representar si el aparcamiento tiene plazas libres" do
+          expect(Parking::IS_FREE).to eq("Parking still has empty spots")
+        end
+        
+        it "Se cuenta con una funcion para mostrar el estado de un aparcamiento (completo, plazas libres)" do
+          expect(@status.status).to eq(Parking::IS_FREE)
+          expect(@statusp2.status).to eq(Parking::IS_COMPLETE)
+          expect { @status.status}.not_to raise_error
+          expect { @status.status(1,2) }.to raise_error(ArgumentError)
+          expect { @status.status(Float)}.to raise_error(ArgumentError)
+          expect { @status.status(String)}.to raise_error(ArgumentError)
+          expect { @status.status(Integer)}.to raise_error(ArgumentError)
+        end
+
+        it "Existe una clase para representar los datos de un aparcamiento" do
+          expect(Parking::Functions).not_to eq(nil)
+          expect(@status.class).to eq(Parking::Functions)
+          expect(@statusp2.class).to eq(Parking::Functions)
+          expect(@status).to be_an_instance_of(Parking::Functions)
+          expect(@statusp2).to be_an_instance_of(Parking::Functions)
+          expect(@status).to be_a(Parking::Functions)
+          expect(@statusp2).to be_a(Parking::Functions)
+          expect(@status).to be_a_kind_of(Parking::Functions)
+        end
+      end
+
+      context "Herencia del módulo Parking" do
+
+        it "Se espera que un aparcamiento sea un objeto de la clase Module" do
+          expect(Parking.class).to eq(Module)
+          expect(Parking.class).to be_a(Module)
+          expect(@v.class).to be_a_kind_of(Module)
+          expect(@m.class).to be_a_kind_of(Module)
+        end
+      end
+
+    end
+    
 
